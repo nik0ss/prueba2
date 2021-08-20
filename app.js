21b396f58023cec7d1cba6ce06315224ef1f58f0
nik0ss <nicolasmartingutierrez@gmail.com>
2021-08-20 02:11:43 -0300

    mi primer commit

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-<!DOCTYPE HTML>
-<html>
-<body>
-<p>Antes del Script</p>
-<script>
-alert( '¡Hola, Mundo!' );
-</script>
-<p>...Después del Script.</p>
-</body>
-</html>
+console.log("HOLA MUNDO");
+LS
